0447510a252ca157bb9171a13b2c1c7474019fc3
Helena Buarque de Macedo <princessgleek@ymail.com>
2015-12-13 17:49:39 -0500

    constructor fixes

--- src/Weights.java
+++ src/Weights.java
@@ -11,9 +11,10 @@ public class Weights
 		coefficient_a = in_coefficient_a;
 		qubit_a = in_qubit_a;
 	}
-	public Weights(int in_qubit_a, int in_qubit_b, int in_coefficient_b)
+	public Weights(int in_qubit_a, int in_qubit_b, int in_coefficient_a)
 	{
-		coefficient_b = in_coefficient_b;
+		coefficient_a = in_coefficient_a;
+		qubit_a = in_qubit_a;
 		qubit_b = in_qubit_b;
 	}
 
